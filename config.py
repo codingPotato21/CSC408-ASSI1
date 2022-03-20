68709aec092233de1c1cbb804aa336f096b5a177
codingPotato69 <adnan.youssef@ymail.com>
2022-03-20 19:03:34 +0400

    Added license header
    - Added license header.
    - Removed the requirements.txt file, will be replaced by manual installation instructions for the required libraries.

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
+## This Bot is based on the examples provided by the Microsoft Azure team.
+## A version of the original code examples can be found on the Microsoft Github-repo here: https://github.com/microsoft/BotBuilder-Samples
+## Licensed under the MIT License.
+
+## - Project Info -
+# Github-repo: https://github.com/codingPotato69/CSC408-ASSI1
+# Contributors: 
+#       1- Adnan Youssef
+#       2- Naji Mohammed
+#       3- Omar Ahmed
+## Licensed under the MIT License.
 
 import os
 
--- dialogs/main_dialog.py
+++ dialogs/main_dialog.py
@@ -1,5 +1,14 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
+## This Bot is based on the examples provided by the Microsoft Azure team.
+## A version of the original code examples can be found on the Microsoft Github-repo here: https://github.com/microsoft/BotBuilder-Samples
+## Licensed under the MIT License.
+
+## - Project Info -
+# Github-repo: https://github.com/codingPotato69/CSC408-ASSI1
+# Contributors: 
+#       1- Adnan Youssef
+#       2- Naji Mohammed
+#       3- Omar Ahmed
+## Licensed under the MIT License.
 
 from botbuilder.dialogs import (
     ComponentDialog,
